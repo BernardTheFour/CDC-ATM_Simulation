85dd9f4405e2cf8a5f743c0071632ddac14e8fdd
BernardTheFour <bernardctxy1@gmail.com>
2022-06-09 14:08:31 +0700

    Other withdraw screen done

--- src/pages/OtherWithdrawScreen.java
+++ src/pages/OtherWithdrawScreen.java
@@ -1,23 +1,59 @@
 package pages;
 
+import java.io.IOException;
+
 import pattern.IState;
+import pattern.Singleton;
 import pattern.StateController;
 
-public class OtherWithdrawScreen extends Page implements IState{
+public class OtherWithdrawScreen extends Page implements IState {
 
     @Override
     public void init(StateController controller) {
-        this.controller = controller;
-        nextPage = Pages.OTHER_WITHDRAW;        
+        super.controller = controller;
     }
 
     @Override
     public void logic() {
-        System.out.println("This is other withdraw screen");        
+        System.out.println("\n--Other Withdraw--");
+        System.out.println("Enter amount to withdraw (max $1000):");
+        System.out.printf("$");
+
+        String answer = super.input.nextLine();
+
+       if (!answer.matches("[0-9]+")){
+           System.out.println("Invalid amount: please input only number");
+           return;
+       }
+
+       int withdraw = Integer.parseInt(answer);
+
+       if (withdraw % 10 != 0){
+           System.out.println("Invalid amount: can only withdraw multiple of 10");
+           return;
+       }
+
+       int balance = Singleton.getLoggedUser().getBalance();
+
+       if (balance < withdraw){
+           System.out.println("Insufficient balance: $" + withdraw);
+           return;
+       }
+
+       Singleton.getLoggedUser().setBalance(balance-withdraw);
+       super.nextPage = Pages.SUMMARY;
     }
 
     @Override
     public void navigate() {
-        // TODO Auto-generated method stub        
-    }    
+        switch (super.nextPage) {
+            case SUMMARY:
+                controller.nextState(Singleton.SummaryScreen());
+                break;
+            default:
+                controller.nextState(controller.getCurrentState());
+                break;
+        }
+        super.nextPage = Pages.DEFAULT;
+    }
 }
