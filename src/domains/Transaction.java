33d618f1fc82e1e144d36beb288f31e4e02c8238
BernardTheFour <bernardctxy1@gmail.com>
2022-06-21 15:39:16 +0700

    refractoring classname

---
+++ src/domains/Transaction.java
@@ -0,0 +1,51 @@
+package domains;
+
+import java.time.LocalDateTime;
+
+public class Transaction {
+
+    public static enum Type{
+        WITHDRAW("WITHDRAW"),
+        TRANSFER("TRANSFER");
+
+        String value;
+
+        Type(String value){
+            this.value = value;
+        }
+    }
+
+    private String accountNumber;
+    private Type transactionType;
+    private String transferTo;
+    private int amount;
+    private LocalDateTime date;
+
+    public String getAccountNumber() {
+        return this.accountNumber;
+    }
+
+    public Type getTransactionType() {
+        return this.transactionType;
+    }
+
+    public String getTransferTo() {
+        return this.transferTo;
+    }
+
+    public int getAmount() {
+        return this.amount;
+    }
+
+    public LocalDateTime getDate() {
+        return this.date;
+    }
+
+    public Transaction(String accountNumber, Type transactionType, String transferTo, int amount, LocalDateTime date) {
+        this.accountNumber = accountNumber;
+        this.transactionType = transactionType;
+        this.transferTo = transferTo;
+        this.amount = amount;
+        this.date = date;
+    }     
+}
